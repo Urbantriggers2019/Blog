b2ff5fcff9cc7718b94ee4b43eaa108c672ee6a5
Urbantriggers2019 <56880537+Urbantriggers2019@users.noreply.github.com>
2019-11-05 09:19:20 +0100

    Dropdown menus
    Got the first dropdown menus to work

--- main.js
+++ main.js
@@ -1,36 +1,57 @@
 $(document).ready(function () {
-    var clicked = false;
     function resizeSmall() {
         if ($(this).width() > 640) {
-            $(".inner_main_menu ul").css("display", "block");
+            $(".inner_main_menu").css("display", "inline");
             $(".menu_container").css("display", "none");
         } else {
-            $(".inner_main_menu ul").css("display", "none");
-            $(".menu_container").css("display", "block");
+            $(".inner_main_menu").css("display", "none");
+            $(".menu_container").css("display", "inline");
         }
     }
     function resizeBig() {
         if ($(this).width() > 1120) {
-            $(".inner_main_menu ul").css("display", "block");
+            $(".inner_main_menu").css("display", "inline");
             $(".menu_container").css("display", "none");
         } else {
-            $(".inner_main_menu ul").css("display", "none");
-            $(".menu_container").css("display", "block");
+            $(".inner_main_menu").css("display", "none");
+            $(".menu_container").css("display", "inline");
         }
     }
     $('.logo').hide();
-    $(".menu li").on('click', function () {
-        $(".menu li").removeClass("active");
-        $(this).addClass("active");
+    var timer;
+    $(".project").on("mouseover", function() {
+        clearTimeout(timer);
+        $(".project > .submenu").addClass("open");
+    }).on("mouseleave", function() {
+        timer = setTimeout(
+            $(".project > .submenu").removeClass("open")
+            , 1000);
+    });
+    $(".research").on("mouseover", function() {
+        clearTimeout(timer);
+        $(".research > .submenu").addClass("open");
+    }).on("mouseleave", function() {
+        timer = setTimeout(
+            $(".research > .submenu").removeClass("open")
+            , 1000);
+    });
+    $(".references").on("mouseover", function() {
+        clearTimeout(timer);
+        $(".references > .submenu").addClass("open");
+    }).on("mouseleave", function() {
+        timer = setTimeout(
+            $(".references > .submenu").removeClass("open")
+            , 1000);
     });
     resizeSmall();
     resizeBig();
+    var clicked = false;
     $(window).scroll(function () {
         $(".main_menu").css("margin-top", Math.max(50 - 0.4 * window.scrollY, 0) + "px");
-        $(".main_menu ul").css("margin-top", Math.max( 20 - 0.02 * window.scrollY, 0) + "px");
-        $(".inner_main_menu ul li").css("font-size", Math.max(35 - 0.02 * window.scrollY, 0) + "px");
-        $(".inner_main_menu ul li:nth-child(2)").css("left", Math.max(15 + 0.026 * window.scrollY, 0) + "%");
-        $(".inner_main_menu ul li:nth-child(3)").css("right", Math.max(20 + 0.026 * window.scrollY, 0) + "%");
+        $("ul.main_menu").css("margin-top", Math.max( 20 - 0.02 * window.scrollY, 0) + "px");
+        $(".inner_main_menu .news, .research, .project, .references").css("font-size", Math.max(35 - 0.02 * window.scrollY, 0) + "px");
+        $(".research").css("left", Math.max(15 + 0.026 * window.scrollY, 0) + "%");
+        $(".project").css("right", Math.max(20 + 0.026 * window.scrollY, 0) + "%");
         $(".bar1, .bar2, .bar3").css("width", Math.max(60 - 0.06 * window.scrollY, 0) + "px");
         $(".bar1, .bar2, .bar3").css("height", Math.max(10 - 0.01 * window.scrollY, 0) + "px");
         if (clicked == false){
