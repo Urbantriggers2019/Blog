783236b13dc66ca037fd7994c046b54e75a3981f
Urbantriggers2019 <56880537+Urbantriggers2019@users.noreply.github.com>
2019-11-15 14:39:13 +0100

    Made map clickable

--- resources/resources.js
+++ resources/resources.js
@@ -5,6 +5,7 @@ window.addEventListener("load", function() {
     $(svg.getElementsByClassName("st0")).on("mouseover", function() {
         for (i = 0; i < svg.querySelectorAll('.st0').length; i++) {
             svg.querySelectorAll('.st0')[i].style.setProperty("opacity", "100%");
+            svg.querySelectorAll('.st0')[i].style.setProperty("cursor", "pointer");
         }
     }).on("mouseleave", function() {
         for (i = 0; i < svg.querySelectorAll('.st0').length; i++) {
@@ -14,6 +15,7 @@ window.addEventListener("load", function() {
     $(svg.getElementsByClassName("st1")).on("mouseover", function() {
         for (i = 0; i < svg.querySelectorAll('.st1').length; i++) {
             svg.querySelectorAll('.st1')[i].style.setProperty("opacity", "100%");
+            svg.querySelectorAll('.st1')[i].style.setProperty("cursor", "pointer");
         }
     }).on("mouseleave", function() {
         for (i = 0; i < svg.querySelectorAll('.st1').length; i++) {
@@ -23,15 +25,19 @@ window.addEventListener("load", function() {
     $(svg.getElementsByClassName("st2")).on("mouseover", function() {
         for (i = 0; i < svg.querySelectorAll('.st2').length; i++) {
             svg.querySelectorAll('.st2')[i].style.setProperty("opacity", "100%");
+            svg.querySelectorAll('.st2')[i].style.setProperty("cursor", "pointer");
         }
     }).on("mouseleave", function() {
         for (i = 0; i < svg.querySelectorAll('.st2').length; i++) {
             svg.querySelectorAll('.st2')[i].style.setProperty("opacity", "50%");
         }
+    }).on("click", function() {
+        window.open("/Blog/resources/forest.html")
     });
     $(svg.getElementsByClassName("st3")).on("mouseover", function() {
         for (i = 0; i < svg.querySelectorAll('.st3').length; i++) {
             svg.querySelectorAll('.st3')[i].style.setProperty("opacity", "100%");
+            svg.querySelectorAll('.st3')[i].style.setProperty("cursor", "pointer");
         }
     }).on("mouseleave", function() {
         for (i = 0; i < svg.querySelectorAll('.st3').length; i++) {
@@ -41,6 +47,7 @@ window.addEventListener("load", function() {
     $(svg.getElementsByClassName("st4")).on("mouseover", function() {
         for (i = 0; i < svg.querySelectorAll('.st4').length; i++) {
             svg.querySelectorAll('.st4')[i].style.setProperty("opacity", "100%");
+            svg.querySelectorAll('.st4')[i].style.setProperty("cursor", "pointer");
         }
     }).on("mouseleave", function() {
         for (i = 0; i < svg.querySelectorAll('.st4').length; i++) {
@@ -50,6 +57,7 @@ window.addEventListener("load", function() {
     $(svg.getElementsByClassName("st5")).on("mouseover", function() {
         for (i = 0; i < svg.querySelectorAll('.st5').length; i++) {
             svg.querySelectorAll('.st5')[i].style.setProperty("opacity", "100%");
+            svg.querySelectorAll('.st5')[i].style.setProperty("cursor", "pointer");
         }
     }).on("mouseleave", function() {
         for (i = 0; i < svg.querySelectorAll('.st5').length; i++) {
@@ -59,29 +67,38 @@ window.addEventListener("load", function() {
     $(svg.getElementsByClassName("st6")).on("mouseover", function() {
         for (i = 0; i < svg.querySelectorAll('.st6').length; i++) {
             svg.querySelectorAll('.st6')[i].style.setProperty("opacity", "100%");
+            svg.querySelectorAll('.st6')[i].style.setProperty("cursor", "pointer");
         }
     }).on("mouseleave", function() {
         for (i = 0; i < svg.querySelectorAll('.st6').length; i++) {
             svg.querySelectorAll('.st6')[i].style.setProperty("opacity", "50%");
         }
+    }).on("click", function() {
+        window.open("/Blog/resources/water.html")
     });
     $(svg.getElementsByClassName("st7")).on("mouseover", function() {
         for (i = 0; i < svg.querySelectorAll('.st7').length; i++) {
             svg.querySelectorAll('.st7')[i].style.setProperty("opacity", "100%");
+            svg.querySelectorAll('.st7')[i].style.setProperty("cursor", "pointer");
         }
     }).on("mouseleave", function() {
         for (i = 0; i < svg.querySelectorAll('.st7').length; i++) {
             svg.querySelectorAll('.st7')[i].style.setProperty("opacity", "50%");
         }
+    }).on("click", function() {
+        window.open("/Blog/resources/mountain.html")
     });
     $(svg.getElementsByClassName("st8")).on("mouseover", function() {
         for (i = 0; i < svg.querySelectorAll('.st8').length; i++) {
             svg.querySelectorAll('.st8')[i].style.setProperty("opacity", "100%");
+            svg.querySelectorAll('.st8')[i].style.setProperty("cursor", "pointer");
         }
     }).on("mouseleave", function() {
         for (i = 0; i < svg.querySelectorAll('.st8').length; i++) {
             svg.querySelectorAll('.st8')[i].style.setProperty("opacity", "50%");
         }
+    }).on("click", function() {
+        window.open("/Blog/resources/land.html")
     });
     var b;
     for (b = 0; b < 9; b++) {
