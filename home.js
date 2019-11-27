10e78a8f681f55d878ede1b0148c6d425122dc75
Urbantriggers2019 <56880537+Urbantriggers2019@users.noreply.github.com>
2019-11-28 00:03:42 +0100

    Changed information about fauske

--- home.js
+++ home.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
     $(" img").on("click", function(){
-        window.location.replace("/Blog/fauske/mastermap.html");
+        window.location.replace("/Blog/fauske/wealth_of_fauske.html");
     });
 });
