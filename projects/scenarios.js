71ecd4d9059f423deb4c501a0bd770e73fba57ad
Urbantriggers2019 <56880537+Urbantriggers2019@users.noreply.github.com>
2019-11-28 12:37:03 +0100

    Made an interactive scenario map

---
+++ projects/scenarios.js
@@ -0,0 +1,36 @@
+window.addEventListener("load", function() {
+    var svgObject = document.getElementById('land').contentDocument;
+    var svg = svgObject.getElementsByTagName('svg')[0];
+    $(svg.getElementsByClassName("cls-1")[0]).on("mouseover", function () {
+        svg.getElementsByClassName("cls-1")[0].style.setProperty("opacity", "100%");
+        svg.getElementsByClassName("cls-1")[0].style.setProperty("cursor", "pointer");
+    }).on("mouseleave", function () {
+        svg.getElementsByClassName("cls-1")[0].style.setProperty("opacity", "80%");
+    }).on("click", function () {
+        window.open("/Blog/projects/marble_park.html", "_self")
+    });
+    $(svg.getElementsByClassName("cls-28")[0]).on("mouseover", function () {
+        svg.getElementsByClassName("cls-28")[0].style.setProperty("opacity", "100%");
+        svg.getElementsByClassName("cls-28")[0].style.setProperty("cursor", "pointer");
+    }).on("mouseleave", function () {
+        svg.getElementsByClassName("cls-28")[0].style.setProperty("opacity", "80%");
+    }).on("click", function () {
+        window.open("/Blog/projects/link.html", "_self")
+    });
+    $(svg.getElementsByClassName("cls-82")[0]).on("mouseover", function () {
+        svg.getElementsByClassName("cls-82")[0].style.setProperty("opacity", "100%");
+        svg.getElementsByClassName("cls-82")[0].style.setProperty("cursor", "pointer");
+    }).on("mouseleave", function () {
+        svg.getElementsByClassName("cls-82")[0].style.setProperty("opacity", "80%");
+    }).on("click", function () {
+        window.open("/Blog/projects/food_festival.html", "_self")
+    });
+    $(svg.getElementsByClassName("cls-142")[0]).on("mouseover", function () {
+        svg.getElementsByClassName("cls-142")[0].style.setProperty("opacity", "100%");
+        svg.getElementsByClassName("cls-142")[0].style.setProperty("cursor", "pointer");
+    }).on("mouseleave", function () {
+        svg.getElementsByClassName("cls-142")[0].style.setProperty("opacity", "80%");
+    }).on("click", function () {
+        window.open("/Blog/projects/roots.html", "_self")
+    });
+});
