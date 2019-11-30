c296def58e16a0b539943a776ee8ea3ff7e20c89
Urbantriggers2019 <56880537+Urbantriggers2019@users.noreply.github.com>
2019-11-30 23:27:24 +0100

    added hub pages and stakeholder diagram

--- projects/scenarios.js
+++ projects/scenarios.js
@@ -15,7 +15,7 @@ window.addEventListener("load", function() {
     }).on("mouseleave", function () {
         svg.getElementsByClassName("cls-28")[0].style.setProperty("opacity", "80%");
     }).on("click", function () {
-        window.open("/Blog/projects/scenarios/link.html", "_self")
+        window.open("/Blog/projects/scenarios/link/overview.html", "_self")
     });
     $(svg.getElementsByClassName("cls-82")[0]).on("mouseover", function () {
         svg.getElementsByClassName("cls-82")[0].style.setProperty("opacity", "100%");
@@ -31,6 +31,6 @@ window.addEventListener("load", function() {
     }).on("mouseleave", function () {
         svg.getElementsByClassName("cls-142")[0].style.setProperty("opacity", "80%");
     }).on("click", function () {
-        window.open("/Blog/projects/scenarios/roots.html", "_self")
+        window.open("/Blog/projects/scenarios/roots/roots_overview.html", "_self")
     });
 });
