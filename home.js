1714e85cc7368ada6125bc5f7b53ed7a47e523bb
Urbantriggers2019 <56880537+Urbantriggers2019@users.noreply.github.com>
2019-12-01 14:15:23 +0100

    update

--- home.js
+++ home.js
@@ -1,5 +1,11 @@
-$(document).ready(function () {
-    $(" img").on("click", function(){
+window.addEventListener("load", function() {
+    console.log("firing");
+    var svgObject = document.getElementById('land').contentDocument;
+    var svg = svgObject.getElementsByTagName('svg')[0];
+    console.log($(svg.getElementsByClassName('cls-2')[0]));
+    $(svg.getElementsByClassName('cls-2')[0]).on("click", function () {
         window.location.replace("/Blog/fauske/wealth_of_fauske.html");
+    }).on("mouseover", function () {
+        document.body.style.cursor = "pointer";
     });
 });
