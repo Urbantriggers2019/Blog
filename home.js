967ac0643085d38792245217d61233cd9c8bb584
Urbantriggers2019 <56880537+Urbantriggers2019@users.noreply.github.com>
2020-02-02 09:31:58 +0100

    Changed the front page and capitalization on first menu items
    Also fixed the energy highlighted main menu item to drivers

--- home.js
+++ home.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", function() {
     var svgObject = document.getElementById('land').contentDocument;
     var svg = svgObject.getElementsByTagName('svg')[0];
-    $(svg.getElementsByClassName('st0')[0]).on("click", function () {
+    $(svg.getElementsByClassName('st27')[0]).on("click", function () {
         window.location.replace("/Blog/fauske/tasks.html");
     }).on("mouseover", function () {
         document.body.style.cursor = "pointer";
@@ -11,12 +11,18 @@ window.addEventListener("load", function() {
         svg.querySelectorAll('#text').style.setProperty("cursor", "pointer");
     });
     $(svg.getElementById('Text')).on("mouseover", function() {
-        for (i = 0; i < svg.querySelectorAll('.st0').length; i++) {
-            svg.querySelectorAll('.st0')[i].style.setProperty("opacity", "100%");
-            svg.querySelectorAll('.st0')[i].style.setProperty("cursor", "pointer");
+        for (i = 0; i < svg.querySelectorAll('.st27').length; i++) {
+            svg.querySelectorAll('.st27')[i].style.setProperty("opacity", "100%");
+            svg.querySelectorAll('.st27')[i].style.setProperty("cursor", "pointer");
         }
+    }).on("mouseleave", function() {
+        for (i = 0; i < svg.querySelectorAll('.st27').length; i++) {
+            svg.querySelectorAll('.st27')[i].style.setProperty("opacity", "80%");
+        }
+    }).on("click", function () {
+        window.location.replace("/Blog/fauske/tasks.html");
     });
-    $(svg.getElementsByClassName('st0')[0]).on("mouseover", function() {
-        svg.querySelectorAll('.st0')[0].style.setProperty("cursor", "pointer");
+    $(svg.getElementsByClassName('st27')[0]).on("mouseover", function() {
+        svg.querySelectorAll('.st27')[0].style.setProperty("cursor", "pointer");
     })
 });
